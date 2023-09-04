5431aa1031c6b97e663b483fb5e6b4d7c88ab5b3
Ivan Soldatov <131395398+ISoldatov@users.noreply.github.com>
2023-09-04 16:30:13 +0300

    fix: refactor model

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,18 +3,26 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 
 @Data
 @AllArgsConstructor
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class Film {
     private Integer id;
-    private final String name;
-    private final String description;
-    private final LocalDate releaseDate;
-    private final int duration;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private int duration;
+
+    public Film(String name, String description, LocalDate releaseDate, int duration) {
+        this.name = name;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+
+    }
 }
