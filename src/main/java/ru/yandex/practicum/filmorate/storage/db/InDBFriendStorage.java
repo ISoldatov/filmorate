5f5196310873fe424144cb9b7f9c94ca2a826e00
Ivan Soldatov <38758452+ivansoldatov@users.noreply.github.com>
2024-01-07 18:39:22 +0300

    style: убраны лишние импорты

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/InDBFriendStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/InDBFriendStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage.db;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
